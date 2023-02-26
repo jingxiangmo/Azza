98314281692242f87149438717338e8d662e2c0f
Walid Aissa <walidaissa047@gmail.com>
2023-02-25 21:27:23 -0500

    integration of the key-phrase extraction model with Gradio

--- app.py
+++ app.py
@@ -1,5 +1,50 @@
 import os
-import gradio as gradio
+import gradio as gr
+
+from transformers import (
+    TokenClassificationPipeline,
+    AutoModelForTokenClassification,
+    AutoTokenizer,
+)
+from transformers.pipelines import AggregationStrategy
+import numpy as np
+
+class KeyphraseExtractionPipeline(TokenClassificationPipeline):
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(
+            model=AutoModelForTokenClassification.from_pretrained(model),
+            tokenizer=AutoTokenizer.from_pretrained(model),
+            *args,
+            **kwargs
+        )
+
+    def postprocess(self, model_outputs):
+        results = super().postprocess(
+            model_outputs=model_outputs,
+            aggregation_strategy=AggregationStrategy.SIMPLE,
+        )
+        return np.unique([result.get("word").strip() for result in results])
+       
+# Load pipeline
+model_name = "ml6team/keyphrase-extraction-kbir-inspec"
+extractor = KeyphraseExtractionPipeline(model=model_name)
+
+# Inference
+def keyphrases_out(input):
+    input = input.replace("\n", " ")
+    keyphrases = extractor(input)
+    out = "The Key Phrases in your text are:\n\n"
+    for k in keyphrases:
+        out += k + "\n"
+    return out
+
+demo = gr.Interface(fn=keyphrases_out, inputs = "text", outputs = "text")
+
+demo.launch()
+
+
+
+
 
 
 
